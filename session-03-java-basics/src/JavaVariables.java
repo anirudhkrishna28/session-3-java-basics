511fadab9fafc1412dbcf0900497dba0643515f6
anirudhkrishna28 <anirudhkrishna.s2021csea@sece.ac.in>
2023-03-11 06:00:03 +0530

    .

---
+++ session-03-java-basics/src/JavaVariables.java
@@ -0,0 +1,53 @@
+package io.javabrains.javabasics;
+
+public class JavaVariables {
+
+    public static void main(String[] args) {
+        // Step 1: Declare and initialize variables of different data types
+        // int variable to store an integer value
+        int a=10;
+        // long variable to store a long value
+        long b = 102222222222222L;
+        // float variable to store a floating-point value
+        float c=10.1f;
+
+        // double variable to store a double value
+        double d=10.2;
+
+        // char variable to store a single character
+        char e = 'A';
+
+        // boolean variable to store a boolean value
+        boolean isday = true;
+
+        // String variable to store a string of characters
+        String f = "Anirudh";
+
+        // Step 2: Print the values of the variables to the console
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(isday);
+        System.out.println(f);
+
+
+        // Step 3: Modify the values of the variables and print the updated values
+        System.out.println("upadted::");
+        a=11;
+        b=1233L;
+        c = 11.1f;
+        d = 11.2;
+        e = 'B';
+        isday=false;
+        f = "Krishna";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(isday);
+        System.out.println(f);
+    }
+}
